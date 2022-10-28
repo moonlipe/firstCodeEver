71bcbaa4ed0410d2ce3fa70ef9c44288aca0a3ca
moonlipe <100380203+moonlipe@users.noreply.github.com>
2022-10-27 21:59:25 -0300

    Add files via upload

---
+++ moonMadlibsTest.py
@@ -0,0 +1,13 @@
+youtuber = "moonlipe"
+print('subscribe to ' + youtuber)
+print(f'subscribe to {youtuber}')
+print('subscribe to {}'.format(youtuber))
+
+adj = input('Adjectiv: ')
+verb1 = input ('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous Person: ')
+madlib = f"Computer programming is so {adj}! It makes me so excited \
+all the time because i love to {verb1}. Stay hydrated and {verb2} \
+like you are {famous_person}!"
+print(madlib)
