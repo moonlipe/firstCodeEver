5128e0b2db7735d57c3a99a832169100318aaca5
Filipe Muniz <100380203+moonlipe@users.noreply.github.com>
2022-10-27 22:39:40 -0300

    Add files via upload

---
+++ moonMadlibs.py
@@ -0,0 +1,8 @@
+import tkinter
+import random
+
+root = Tk()
+root.geometry('300X300')
+root.title('DataFlair-Mad Libs Generator')
+Label(root, text= 'Mad Libs Generator \n Have Fun!')
+Label(root, text= 'Click Any One: ', font = 'arial 15 bold'.place(x=40, y=80))
